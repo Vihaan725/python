ebd4d5c5b046fd82eab09e6fc67798b1ef37d647
Vihaan725 <vihaanveks725@gmail.com>
2026-03-12 19:37:03 -0400

    Add files via upload

---
+++ operation1.py
@@ -0,0 +1,8 @@
+print("Enter marks in 4 subjects")
+s1 = int(input())
+s2 = int(input())
+s3 = int(input())
+s4 = int(input())
+
+percentage = ((s1+s2+s3+s4)/400)*100
+print(percentage)
---
+++ operators.py
@@ -0,0 +1,8 @@
+a = 98
+b = 94
+c = 41
+d = 96
+e = 11
+
+mean = (a+b+c+d+e)/5
+print(mean)
