5db1ce6e945d62063962d5a6af6c32d55cf46ac7
Vihaan725 <vihaanveks725@gmail.com>
2026-05-28 18:56:48 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(p,q):
+    return p+q
+def substract(p,q):
+    return p-q
+def multiply(p,q):
+    return p*q
+def divide(p,q):
+    return p/q
+a = add(5,6)
+b = substract(5,6)
+c = multiply(5,6)
+d = divide(5,6)
+print(a,b,c,d)
---
+++ weather.py
@@ -0,0 +1,6 @@
+def weather():
+    print("weather is pleasent in", s)
+    print("weather is same in ", a)
+a = "autumn"
+s = "spring"
+weather()
