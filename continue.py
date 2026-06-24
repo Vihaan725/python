02e9af8a587069cb39b3b0f009d0fdfd12f34da3
Vihaan725 <vihaanveks725@gmail.com>
2026-06-24 19:04:43 -0400

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,13 @@
+for c in range(1,11):
+    if c==5:
+        continue
+    else:
+        print(c)
+
+var = 10
+while var > 0:
+    var = var - 1
+    if var = 5
+        continue
+    print('\nCurrent variable value:', var)
+print("\nGoodbye")
