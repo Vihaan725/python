08206f1c4245a2be4b55153423cb40c2bc71d208
Vihaan725 <vihaanveks725@gmail.com>
2026-07-15 19:15:01 -0400

    Add files via upload

---
+++ exam#2.py
@@ -0,0 +1,27 @@
+def add(a, b):
+    return (a+b)
+def substract(a, b):
+    return (a-b)
+def multiply(a, b):
+    return (a*b)
+def divide(a, b):
+    return (a/b)
+
+print("1 = add, 2 = substract, 3 = multiply, 4 = divide")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+1 == add
+2 == substract
+3 == multiply
+4 == divide
+function = int(input("Enter the number 1,2,3, or 4 to do the operation: "))
+
+if function == 1:
+    print(a+b)
+elif function == 2:
+    print(a-b)
+elif function == 3:
+    print(a*b)
+else:
+    print(a/b)
+
