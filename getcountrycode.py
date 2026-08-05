a52c2de73aa9d51eb57c5cd7cbb357474c029396
Vihaan725 <vihaanveks725@gmail.com>
2026-08-05 19:01:25 -0400

    Add files via upload

---
+++ getcountrycode.py
@@ -0,0 +1,9 @@
+country_code = {'India' : '1956',
+                'England' : '1966',
+                'USA' : '2025'}
+
+print('Country code for India - ')
+print(country_code.get('India', 'Not Found'))
+
+print('Country code for Netherlands - ')
+print(country_code.get('Netherlands', 'Not Found'))
